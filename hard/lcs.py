a7951fa38f7f9a9f65bd0848645229314e8c7a97
Zack Newman <znewman01@gmail.com>
2013-10-18 18:11:38 -0400

    s/substring/subsequence/g

--- hard/lcs.py
+++ hard/lcs.py
@@ -1,4 +1,4 @@
-# Find the longest common substring between two strings.  ie. for “abcde” and
-# “acccde” the longest common substring is “acde”.
+# Find the longest common subsequence between two strings.  ie. for “abcde” and
+# “acccde” the longest common subsequence is “acde”.
 
 
